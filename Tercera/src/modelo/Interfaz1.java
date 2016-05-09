ab956729767b3210b1813d9f8b267e4f225ba567
Talanty <BrulLgmsn@gmail.com>
2016-05-09 19:18:45 +0100

    Add files via upload
    mi proyecto a fecha 9-5-2016

---
+++ Tercera/src/modelo/Interfaz1.java
@@ -0,0 +1,12 @@
+package modelo;
+// comentario Git
+public interface Interfaz1 {
+	public int metodo1(String param1, int param2);
+
+	public void metodo2(Float param1);
+
+	public Integer[] metodo3(float param1);
+
+	public int propiedad1=0;
+
+}
